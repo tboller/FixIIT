4a617edb5b71e24657cc3e479123a5e67d89845d
Emmanuel Zambrano <emmanuelzambrano@joses-imac-756.attlocal.net>
2018-01-31 22:49:53 -0600

    created skeleton app

--- FixMe/app.js
+++ FixMe/app.js
@@ -10,9 +10,6 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,7 +17,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/', express.static(path.join(__dirname, 'dist')));
 
 app.use('/', index);
 app.use('/users', users);
