7c3358a2ea7d37d727de57b1573ce0fd62eb25a8
Emmanuel <ezambran@hawk.iit.edu>
2018-03-10 17:19:59 -0600

    Routing in angular

--- FixMe/angular-app/src/app/app.module.ts
+++ FixMe/angular-app/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule, Routes } from '@angular/router';
+	
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserComponent } from './user/user.component';
 import { AdminComponent } from './admin/admin.component';
 
+const appRoutes: Routes = [
+	{ path: '', component: LoginComponent},
+	{ path: 'register', component: RegisterComponent}
+]
 
 @NgModule({
   declarations: [
@@ -18,6 +22,10 @@ import { AdminComponent } from './admin/admin.component';
     AdminComponent
   ],
   imports: [
+		RouterModule.forRoot(
+			appRoutes,
+			{ enableTracing: true }
+		),
     BrowserModule
   ],
   providers: [],
--- FixMe/app.js
+++ FixMe/app.js
@@ -68,7 +68,9 @@ app.get('/', (req, res) => {
   res.send('invaild endpoint');
 });
 
-
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 // 404 error cather
 app.use(function(req, res, next) {
