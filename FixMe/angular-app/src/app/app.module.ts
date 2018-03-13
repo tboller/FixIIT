a1adc681098120caacba89240ebbbb6cc9252caa
Emmanuel <ezambran@hawk.iit.edu>
2018-03-12 20:52:20 -0500

    Updated routing paths in angular

--- FixMe/angular-app/src/app/app.module.ts
+++ FixMe/angular-app/src/app/app.module.ts
@@ -10,8 +10,9 @@ import { AdminComponent } from './admin/admin.component';
 import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
-	{ path: '', component: LoginComponent},
-	{ path: 'register', component: RegisterComponent}
+	{ path: '', component: HomeComponent },
+	{ path: 'login', component: LoginComponent },
+	{ path: 'register', component: RegisterComponent }
 ]
 
 @NgModule({
