f8d6348e2d78b9b2202f1151474d24b1e4991775
Tboller <tboller@hawk.iit.edu>
2018-04-10 10:22:52 -0500

    Made index router links into buttons

--- FixMe/angular-app/src/app/auth/auth.service.ts
+++ FixMe/angular-app/src/app/auth/auth.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from "@angular/core";
 import { Http } from "@angular/http";
+import {Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 import { User } from "./user.model";
 
 @Injectable()
 export class AuthService {
+    authToken: any;
+    user: any;
+    
     constructor(private http: Http) {}
 
     register(user: User) {
