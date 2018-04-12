18721f055196765536f4a8cb2f969582b6697b29
Emmanuel Zambrano <ezambran@hawk.iit.edu>
2018-04-11 21:07:58 -0500

    API for creating tickets

--- FixMe/models/ticket.js
+++ FixMe/models/ticket.js
@@ -5,7 +5,18 @@ var Schema = mongoose.Schema;
 
 var TicketSchema = Schema({
 	title: {type: String, required: true},
-	description: {type: String, required: true}
+	description: {type: String, required: true},
+	link: {type: String},
+	like: {type: Number, required: true, default: 0},
+	dislike: {type: Number, required: true, default: 0}
 });
 
 var Ticket = module.exports = mongoose.model('Ticket', TicketSchema);
+
+module.exports.addTicket = function(newTicket, callback){
+	newTicket.save(callback);
+}
+
+module.exports.getTicketById = function(id, callback) {
+  Ticket.findById(id, callback);
+}
--- FixMe/routes/tickets.js
+++ FixMe/routes/tickets.js
@@ -3,27 +3,66 @@ var router = express.Router();
 var passport = require('passport');
 var jwt = require('jsonwebtoken');
 var mongo = require('../config/mongo');
-
-//test route
+var Ticket = require('../models/ticket');
 
 router.get('/', function(req, res, next){
-	//get All Tickets	
-	res.send("All tickets");
+	Ticket.find({}).exec(function (err, tickets) {
+     if (err) {
+       return next(err);
+     }
+     res.json(tickets);
+   });
 });
 
 router.post('/', function(req,res,next){
-	//new ticket
+	var newTicket = new Ticket({
+    title: req.body.title,
+    description: req.body.description,
+    link: req.body.link,
+  });
+	console.log(newTicket);
+
+  Ticket.addTicket(newTicket, (err, ticket) =>{
+      if(err){
+        res.json({success: false, msg: 'Failed to Create Ticket'})
+      }else {
+        res.json(ticket);
+      }
+  });
 });
 
 router.get('/:id', function(req, res, next){
-  //get One ticket 
-  res.send("One ticket");
+	Ticket.getTicketById(req.params.id, (err, ticket) =>{
+		if(err){
+			res.json({success: false, msg: 'No Ticket Exists'})
+		}else {
+			res.json(ticket)
+		}
+	});
 });
 
 router.put('/:id', function(req, res, next){
-  //edit (voting)
+	var editedTicket = new Ticket({
+		_id: req.params.id,
+    title: req.body.title,
+    description: req.body.description,
+  });
+
+	Ticket.findOneAndUpdate({_id: req.params.id}, editedTicket , function(err, ticket) {
+    if (err){
+			res.json({success: false, msg: 'Failed to Register'})
+    }
+    res.json(ticket);
+  });
+});
+
+router.delete('/', function(req, res, next){
+	Ticket.remove({}, function(err, ticket) {
+		if (err)
+			res.json({succes: false, msg: 'Failed to delete'});
+		res.json({ message: 'Tickets successfully deleted' });
+	});
 });
 
 
-router.get
 module.exports = router;
